b409d0b5ef9ec050553b7dc672f429cd03bb7cc5
Eric Song <ericksong91@gmail.com>
2022-07-26 12:17:30 -0400

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(capitalized){
+    return customerName = "BOB";
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "bobby"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "eddy"
+}
+
+console.log(leastFavoriteCustomer);
+
+console.log(changeLeastFavoriteCustomer());
